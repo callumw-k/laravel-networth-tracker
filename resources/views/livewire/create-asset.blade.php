52ac68745156cbf445295d221828dba5f9f27b4f
Callum Wellwood-Kane <business@callumwk.com>
2024-08-13 12:52:06 +1000

    updated input fields

--- resources/views/livewire/create-asset.blade.php
+++ resources/views/livewire/create-asset.blade.php
@@ -1,9 +1,13 @@
 <div>
     <form wire:submit="createAsset">
-        <div class="grid grid-cols-3 border border-black border-t-0 ">
-            <input wire:model="assetName" class="border-r border-r-black p-2" placeholder="Asset name">
-            <input wire:model="assetValue" class="p-2" type="number" placeholder="value">
-            <button type="submit"></button>
+        <div class="grid grid-cols-[2fr,1fr,40px] border border-black border-t-0 ">
+            <input wire:model="assetName" class="border-r border-r-black p-2 min-w-0" placeholder="Asset name">
+            <input wire:model="assetValue" class="px-2 w-auto min-w-0" placeholder="Value">
+            <div class="px-2 flex justify-center border-l-black border-l">
+                <button type="submit">
+                    <x-heroicon-c-plus class="size-6"/>
+                </button>
+            </div>
         </div>
     </form>
 </div>
--- resources/views/livewire/view-assets.blade.php
+++ resources/views/livewire/view-assets.blade.php
@@ -1,13 +1,14 @@
 <div>
     @foreach($assets as $asset)
         <div wire:key="$asset->id"
-             class="grid grid-cols-2 w-full border border-black border-t-0 first:border-t ">
+             class="grid grid-cols-[2fr,1fr,40px] w-full border border-black border-t-0 first:border-t ">
             <div class="p-2 border-r-black border-r">
                 <p> {{ $asset->name }} </p>
             </div>
             <div class="p-2">
                 <p>{{$asset->value}}</p>
             </div>
+            <div class="border-l border-l-black"></div>
         </div>
     @endforeach
 </div>
